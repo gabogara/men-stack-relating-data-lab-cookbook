8234f24c586369baa3be99917aacc38e46213227
gabogara <gabo1gara@gmail.com>
2025-12-09 19:03:31 -0800

    add shared navbar partial with auth-aware links

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const session = require("express-session");
 const authController = require("./controllers/auth.js");
 const recipesController = require("./controllers/recipes.js");
 const ingredientsController = require("./controllers/ingredients.js");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
 
 const port = process.env.PORT ? process.env.PORT : "3000";
 
@@ -30,10 +32,15 @@ app.use(
   })
 );
 
+app.use(passUserToView);
+app.use("/auth", authController);
+
+app.use(isSignedIn);
+app.use("/recipes", recipesController);
+app.use("/ingredients", ingredientsController);
+
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    user: req.session.user,
-  });
+  res.render("index.ejs");
 });
 
 app.get("/vip-lounge", (req, res) => {
@@ -43,10 +50,6 @@ app.get("/vip-lounge", (req, res) => {
     res.send("Sorry, no guests allowed.");
   }
 });
-
-app.use("/auth", authController);
-app.use("/recipes", recipesController);
-app.use("/ingredients", ingredientsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
