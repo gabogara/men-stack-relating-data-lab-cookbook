468d6a5745c75782e05163c4b77ede2dbef59f83
gabogara <gabo1gara@gmail.com>
2025-12-09 22:13:34 -0800

    implement recipes new and create routes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const User = require("../models/user.js");
 const Recipe = require("../models/recipe.js");
+const Ingredient = require("../models/ingredient.js");
 
 router.get("/", async (req, res) => {
   try {
@@ -12,6 +13,18 @@ router.get("/", async (req, res) => {
     console.log(error);
     res.redirect("/");
   }
+});
+
+router.get("/new", async (req, res) => {
+  const ingredients = await Ingredient.find({});
+  res.render("recipes/new.ejs", { ingredients });
+});
+
+router.post("/", async (req, res) => {
+  const newRecipe = new Recipe(req.body);
+  newRecipe.owner = req.session.user._id;
+  await newRecipe.save();
+  res.redirect("/recipes");
 });
 
 module.exports = router;
