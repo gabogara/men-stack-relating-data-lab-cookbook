ce91d277d25b000050160d2ad87c3c1cbe0249bc
gabogara <gabo1gara@gmail.com>
2025-12-09 16:01:03 -0800

    add Recipe and Ingredient models

---
+++ models/recipe.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  instructions: String,
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  ingredients: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ingredient",
+    },
+  ],
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
