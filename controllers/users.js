600636ae6ed016e85b17f698ac498fbdeb984dcb
gabogara <gabo1gara@gmail.com>
2025-12-11 01:51:18 -0800

    Add community index view listing all users

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user.js");
+
+// USERS INDEX PAGE
+// GET /users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.render("users/index.ejs", { users });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+module.exports = router;
