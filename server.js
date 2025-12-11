91b1d3804f371541f3ff1b184d9b2fd55df6ef34
gabogara <gabo1gara@gmail.com>
2025-12-11 01:11:07 -0800

    Remove VIP lounge link and route

--- server.js
+++ server.js
@@ -43,14 +43,6 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/vip-lounge", (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send("Sorry, no guests allowed.");
-  }
-});
-
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
