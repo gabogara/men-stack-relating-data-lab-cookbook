c4aa92c2fa87fdfa4ecb1ab96131c07133ee11d2
gabogara <gabo1gara@gmail.com>
2025-12-10 23:57:07 -0800

    Prevent duplicate ingredient names at schema level

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 const ingredientSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
 });
 
 const Ingredient = mongoose.model("Ingredient", ingredientSchema);
